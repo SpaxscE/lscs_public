ab0853c806bc47cd004fb0170ab9ce48cd374f81
Luna <swagnemite@gmx.de>
2022-06-01 19:01:08 +0200

    dfg

--- lua/lscs/autorun/cl_menu.lua
+++ lua/lscs/autorun/cl_menu.lua
@@ -67,19 +67,19 @@ end
 
 local Frame
 
-local FrameBarHeight = 128
+local FrameBarHeight = 24
 
 local FrameSizeX = 750
-local FrameSizeY = 456
+local FrameSizeY = 400 + FrameBarHeight
 
-local SelectorHeight = FrameSizeY - FrameBarHeight
+local SelectorHeight = 80 * 5
 local SelectorWidth = 80
 local SelectorWidthActive = 196
 
 local PanelPosX = SelectorWidth
 local PanelPosY = FrameBarHeight
 local PanelSizeX = FrameSizeX - SelectorWidth
-local PanelSizeY = FrameSizeY - FrameBarHeight
+local PanelSizeY = FrameSizeY - FrameBarHeight * 2
 
 function LSCS:SetActivePanel( newpanel )
 	if not IsValid( Frame ) then
@@ -93,6 +93,41 @@ function LSCS:SetActivePanel( newpanel )
 	Frame.PANEL = newpanel
 end
 
+local function BaseButtonClickSB( self, sound )
+	sound = sound or "ui/buttonclick.wav"
+
+	surface.PlaySound( sound )
+
+	Frame.buttons = Frame.buttons or {}
+	Frame.buttons[ self.ID ] = 1
+end
+
+local function DrawButtonClickSB( self, w, h ) 
+	local Col = menu_white_dim
+	if self:IsHovered() then
+		Col = menu_white
+	end
+
+	Frame.buttons = Frame.buttons or {}
+	Frame.buttons[ self.ID ] = Frame.buttons[ self.ID ] or 0
+
+	Frame.buttons[ self.ID ] = Frame.buttons[ self.ID ] - math.min(Frame.buttons[ self.ID ],RealFrameTime() * 3)
+
+	local Size = Frame.buttons[ self.ID ]
+
+	if Size > 0.05 then
+		surface.DrawCircle( w * 0.5, h * 0.5, (1 - Size) * (w - 25), 150, 200, 255, 255 * Size )
+		surface.DrawCircle( w * 0.5, h * 0.5, (1 - Size) * (w - 15), 150, 200, 255, 255 * 0.5 * Size )
+		surface.DrawCircle( w * 0.5, h * 0.5, (1 - Size) * (w - 10), 150, 200, 255, 255 * 0.2 * Size )
+
+		surface.SetMaterial( ClickMat )
+		surface.SetDrawColor( 150 * Size, 200 * Size, 255 * Size, 255 * Size )
+		surface.DrawTexturedRect( w * 0.5 - w * 0.5 * Size * 2, h * 0.5 - h * 0.5 * Size * 2, w * Size * 2, h * Size * 2 )
+	end
+
+	return Col
+end
+
 local function CreateSideBarButton( icon, ID, text )
 	local button = vgui.Create( "Button", Frame.SideBar )
 	button.text = text or ""
@@ -100,15 +135,13 @@ local function CreateSideBarButton( icon, ID, text )
 	button:SetSize( SelectorWidthActive,  80 )
 	button:SetPos( 0,  (ID - 1) * 80 )
 	button.DoClick = function( self )
-		BaseButtonClick( self )
-		Frame.ID = self.ID
 	end
 	button.Paint = function(self, w, h )
-		Frame.SideBar._smSB = Frame.SideBar._smSB or SelectorWidth
+		Frame._smSB = Frame._smSB or SelectorWidth
 
-		local xPos = Frame.SideBar._smSB - 8 - 64
+		local xPos = Frame._smSB - 8 - 64
 
-		local Col = DrawButtonClick( self, w, h ) 
+		local Col = DrawButtonClickSB( self, w, h ) 
 		if Frame.ID == self.ID then
 			Col = menu_text
 
@@ -136,49 +169,40 @@ function LSCS:SideBar( Frame )
 	end
 	Frame.SideBar = vgui.Create( "DPanel", Frame )
 	Frame.SideBar:SetPos( 0, FrameBarHeight )
-	Frame.SideBar:SetSize( SelectorWidth, SelectorHeight )
+	Frame.SideBar:SetSize( (Frame:SideBarIsActive() and SelectorWidthActive or SelectorWidth), SelectorHeight )
 	Frame.SideBar.Paint = function(self, w, h )
 		draw.RoundedBoxEx( 8, 0, 0, w, h, menu_dark, false, false, true, false )
 	end
-	Frame.SideBar.IsActivated = function( self )
-		return self._smSB == SelectorWidthActive
-	end
-	Frame.SideBar.IsActive = function( self )
-		return self._isActive == true
-	end
-	Frame.SideBar.SetActive = function( self, active )
-		self._isActive = active
-	end
-	Frame.SideBar.Think = function( self )
-		local Rate = RealFrameTime() * 1000
-		local Active = self:IsActive()
-		local TargetWidth = Active and SelectorWidthActive or SelectorWidth
 
-		local X, Y = self:CursorPos()
-
-		if Active then
-			if X < 0 or X > SelectorWidthActive or Y < 0 or Y > FrameSizeY then
-				self:SetActive( false )
-			end
-		else
-			if X > 0 and X < SelectorWidth and Y > 0 and Y < FrameSizeY then
-				self:SetActive( true )
-			end
-		end
-
-		self._smSB = self._smSB and (self._smSB + math.Clamp(TargetWidth - self._smSB,-Rate,Rate)) or SelectorWidth
-
-		if self._smSB ~= self.old_smSB then
-			self.old_smSB = self._smSB
-
-			self:SetSize( self._smSB, SelectorHeight )
-		end
+	local button = CreateSideBarButton( icon_lscs, 1, "Home" )
+	button.DoClick = function( self )
+		BaseButtonClickSB( self )
+		LSCS:BuildMainMenu( Frame )
 	end
 
-	local button = CreateSideBarButton( icon_inventory, 1, "Inventory" )
-	local button = CreateSideBarButton( icon_hilt, 2, "Lightsaber" )
-	local button = CreateSideBarButton( icon_stance, 3, "Stance" )
-	local button = CreateSideBarButton( icon_settings, 4, "Settings" )
+	local button = CreateSideBarButton( icon_inventory, 2, "Inventory" )
+	button.DoClick = function( self )
+		BaseButtonClickSB( self )
+		LSCS:BuildInventory( Frame )
+	end
+
+	local button = CreateSideBarButton( icon_hilt, 3, "Lightsaber" )
+	button.DoClick = function( self )
+		BaseButtonClickSB( self )
+		LSCS:BuildSaberMenu( Frame )
+	end
+
+	local button = CreateSideBarButton( icon_stance, 4, "Stance" )
+	button.DoClick = function( self )
+		BaseButtonClickSB( self )
+		LSCS:BuildStanceMenu( Frame )
+	end
+
+	local button = CreateSideBarButton( icon_settings, 5, "Settings" )
+		button.DoClick = function( self )
+		BaseButtonClickSB( self )
+		LSCS:BuildSettings( Frame )
+	end
 end
 
 function LSCS:BuildMainMenu( Frame )
@@ -186,13 +210,91 @@ function LSCS:BuildMainMenu( Frame )
 	Panel:SetPos( PanelPosX, PanelPosY )
 	Panel:SetSize( PanelSizeX, PanelSizeY )
 	Panel.Paint = function(self, w, h )
-		--local Col = menu_dim
-		--surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
-		--surface.DrawRect( 0, 0, w, h  )
+		local Col = menu_light
+		surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+		surface.DrawRect( 0, 0, w, h  )
 	end
 
-	LSCS:SideBar( Frame )
 	LSCS:SetActivePanel( Panel )
+	LSCS:SideBar( Frame )
+
+	Frame.ID = 1
+
+	local DermaButton = vgui.Create( "DButton", Panel )
+	DermaButton:SetText( "Say hi" )
+	DermaButton:SetPos( 25, 50 )
+	DermaButton:SetSize( 250, 30 )
+	DermaButton.DoClick = function()
+		local menu = DermaMenu()
+		menu:AddOption( "Copy to clipboard", function() SetClipboardText( "weapon_pistol" ) end )
+		menu:AddOption( "Spawn 5", function() for i=1,5 do RunConsoleCommand( "gm_spawnswep", "weapon_pistol" ) end end )
+		menu:AddOption( "Spawn 10", function() for i=1,10 do RunConsoleCommand( "gm_spawnswep", "weapon_pistol" ) end end )
+		menu:Open()
+	end
+end
+
+function LSCS:BuildInventory( Frame )
+	local Panel = vgui.Create( "DPanel", Frame )
+	Panel:SetPos( PanelPosX, PanelPosY )
+	Panel:SetSize( PanelSizeX, PanelSizeY )
+	Panel.Paint = function(self, w, h )
+		local Col = menu_light
+		surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+		surface.DrawRect( 0, 0, w, h  )
+	end
+
+	LSCS:SetActivePanel( Panel )
+	LSCS:SideBar( Frame )
+
+	Frame.ID = 2
+end
+
+function LSCS:BuildSaberMenu( Frame )
+	local Panel = vgui.Create( "DPanel", Frame )
+	Panel:SetPos( PanelPosX, PanelPosY )
+	Panel:SetSize( PanelSizeX, PanelSizeY )
+	Panel.Paint = function(self, w, h )
+		local Col = menu_light
+		surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+		surface.DrawRect( 0, 0, w, h  )
+	end
+
+	LSCS:SetActivePanel( Panel )
+	LSCS:SideBar( Frame )
+
+	Frame.ID = 3
+end
+
+function LSCS:BuildStanceMenu( Frame )
+	local Panel = vgui.Create( "DPanel", Frame )
+	Panel:SetPos( PanelPosX, PanelPosY )
+	Panel:SetSize( PanelSizeX, PanelSizeY )
+	Panel.Paint = function(self, w, h )
+		local Col = menu_light
+		surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+		surface.DrawRect( 0, 0, w, h  )
+	end
+
+	LSCS:SetActivePanel( Panel )
+	LSCS:SideBar( Frame )
+
+	Frame.ID = 4
+end
+
+function LSCS:BuildSettings( Frame )
+	local Panel = vgui.Create( "DPanel", Frame )
+	Panel:SetPos( PanelPosX, PanelPosY )
+	Panel:SetSize( PanelSizeX, PanelSizeY )
+	Panel.Paint = function(self, w, h )
+		local Col = menu_light
+		surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+		surface.DrawRect( 0, 0, w, h  )
+	end
+
+	LSCS:SetActivePanel( Panel )
+	LSCS:SideBar( Frame )
+
+	Frame.ID = 5
 end
 
 function LSCS:OpenMenu()
@@ -204,49 +306,53 @@ function LSCS:OpenMenu()
 		Frame:SetScreenLock( true )
 		Frame:MakePopup()
 		Frame:Center()
-		Frame:ShowCloseButton( false )
 		Frame.Paint = function(self, w, h )
 			draw.RoundedBox( 8, 0, 0, w, h, menu_light )
+			draw.RoundedBoxEx( 8, 0, 0, w, FrameBarHeight, menu_dark, true, true, false, false)
+			draw.SimpleText( "[LSCS] - Control Panel ", "LSCS_FONT", 5, 11, menu_light, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
 		end
 		Frame.GetSideBar = function(self)
 			return self.SideBar 
 		end
-
-		local button = vgui.Create( "Button", Frame )
-		button:SetText( "" )	
-		button:SetSize( 32, 32 )
-		button:SetPos( FrameSizeX - 48,  16 )
-		button.Frame = Frame
-		button.DoClick = function( self )
-			BaseButtonClick( self )
-			self.Frame:Remove()
+		Frame.SideBarIsActivated = function( self )
+			return self._smSB == SelectorWidthActive
 		end
-		button.Paint = function(self, w, h )
-			local Col = DrawButtonClick( self, w, h ) 
-
-			surface.SetMaterial( icon_close )
-			surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
-			surface.DrawTexturedRect( w * 0.5 - 12, h * 0.5 - 12, 24, 24 )
+		Frame.SideBarIsActive = function( self )
+			return self._isActive == true
 		end
-
-		local button = vgui.Create( "Button", Frame )
-		button:SetText( "" )	
-		button:SetSize( FrameBarHeight,  FrameBarHeight )
-		button:SetPos( 0, 0 )
-		button.DoClick = function( self )
-			BaseButtonClick( self )
-			gui.OpenURL( "https://steamcommunity.com/id/Blu-x92/" )
+		Frame.SideBarSetActive = function( self, active )
+			self._isActive = active
 		end
-		button.Paint = function(self, w, h )
-			draw.RoundedBoxEx( 8, 0, 0, w, h, menu_dark, true, false, false, false )
+		Frame.oldThink = Frame.Think
+		Frame.Think = function( self )
+			self:oldThink()
 
-			local Col = DrawButtonClick( self, w, h ) 
-			surface.SetMaterial( icon_lscs )
-			surface.SetDrawColor( Col.r, Col.g, Col.b, Col.a )
+			local Rate = RealFrameTime() * 1000
+			local Active = self:SideBarIsActive()
+			local TargetWidth = Active and SelectorWidthActive or SelectorWidth
 
-			local Dist = 17
-			local Size = FrameBarHeight - Dist * 2
-			surface.DrawTexturedRect( Dist, Dist, Size, Size )
+			local X, Y = self:CursorPos()
+
+			if Active then
+				if X < 0 or X > SelectorWidthActive or Y < 0 or Y > FrameSizeY then
+					self:SideBarSetActive( false )
+				end
+			else
+				if X > 0 and X < SelectorWidth and Y > 0 and Y < FrameSizeY then
+					self:SideBarSetActive( true )
+				end
+			end
+
+			self._smSB = self._smSB and (self._smSB + math.Clamp(TargetWidth - self._smSB,-Rate,Rate)) or SelectorWidth
+
+			if self._smSB ~= self.old_smSB then
+				self.old_smSB = self._smSB
+
+				local SB = self:GetSideBar()
+				if IsValid( SB ) then
+					SB:SetSize( self._smSB, SelectorHeight )
+				end
+			end
 		end
 
 		LSCS:BuildMainMenu( Frame )
