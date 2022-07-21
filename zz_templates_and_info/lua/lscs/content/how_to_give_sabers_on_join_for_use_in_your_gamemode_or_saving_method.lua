ed473756dc8e69c3ab52e5e54fbee80bb1e15259
Luna <swagnemite@gmx.de>
2022-07-22 00:46:37 +0200

    yee

--- zz_templates_and_info/lua/lscs/content/how_to_give_sabers_on_join_for_use_in_your_gamemode_or_saving_method.lua
+++ zz_templates_and_info/lua/lscs/content/how_to_give_sabers_on_join_for_use_in_your_gamemode_or_saving_method.lua
@@ -13,10 +13,24 @@ hook.Add( "LSCS:OnPlayerFullySpawned", "ANY_HOOK_NAME_YOU_WANT", function( ply )
 	inventory[ 1 ] = "item_saberhilt_katarn"
 	inventory[ 2 ] = "item_crystal_sapphire"
 	inventory[ 3 ] = "item_stance_yongli"
+	inventory[ 4 ] = "item_force_heal"
+	inventory[ 5 ] = "item_force_immunity"
+	inventory[ 6 ] = "item_force_jump"
+	inventory[ 7 ] = "item_force_pull"
+	inventory[ 8 ] = "item_force_push"
+	inventory[ 9 ] = "item_force_replenish"
+	inventory[ 10 ] = "item_force_sense"
 
 	equipped[ 1 ] = true -- false would be left hand
 	equipped[ 2 ] = true -- false would be left hand
 	equipped[ 3 ] = true -- a saber stance can only be right hand, and so do forcepowers
+	equipped[ 4 ] = true
+	equipped[ 5 ] = true
+	equipped[ 6 ] = true
+	equipped[ 7 ] = true
+	equipped[ 8 ] = true
+	equipped[ 9 ] = true
+	equipped[ 10 ] = true
 
 	ply:lscsSyncInventory()
 	ply:lscsBuildPlayerInfo()
