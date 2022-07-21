5b3cf1c67736245997b81233ce9d5671c3d7c6ee
Luna <swagnemite@gmx.de>
2022-07-21 20:31:31 +0200

    another sample

--- zz_templates_and_info/lua/lscs/content/how_to_give_sabers_on_join_for_use_in_your_gamemode_or_saving_method.lua
+++ zz_templates_and_info/lua/lscs/content/how_to_give_sabers_on_join_for_use_in_your_gamemode_or_saving_method.lua
@@ -13,3 +13,40 @@ hook.Add( "PlayerInitialSpawn", "auto_equip_on_join", function( ply )
 
 	-- If you give the items outside PlayerInitialSpawn you will need to call ply:lscsCraftSaber() to force the player to craft a lightsaber
 end )
+
+-- if you need control over which hand its a little more complicated:
+--[[
+
+hook.Add( "PlayerInitialSpawn", "auto_equip_on_join", function( ply )
+	local inventory = ply:lscsGetInventory()
+	local equipped = ply:lscsGetEquipped()
+
+	table.Empty( inventory ) -- clean the inventory, just in case
+	table.Empty( equipped ) -- clean the equipped list, just in case
+
+	inventory[ 1 ] = "item_saberhilt_katarn"
+	inventory[ 2 ] = "item_crystal_sapphire"
+	inventory[ 3 ] = "item_stance_yongli"
+	--[[
+	alternative to above. However you have no control over what ID they end up being using ply:Give and it will physically spawn a entity that the player will pick up on touch so there is always a chance of it failing
+	ply:Give("item_saberhilt_katarn")
+	ply:Give("item_crystal_sapphire")
+	ply:Give("item_stance_yongli")
+	]]
+
+	equipped[ 1 ] = true -- false would be left hand
+	equipped[ 2 ] = true -- false would be left hand
+	equipped[ 3 ] = true -- a saber stance can only be right hand, and so do forcepowers
+
+	ply:lscsBuildPlayerInfo() -- networks all the data and syncs it with client
+
+	-- if you do all this outside of PlayerInitialSpawn AFTER the hook LSCS:OnPlayerFullySpawned is ran, you need to call ply:lscsSyncInventory() yourself.
+	-- In this example here it is not needed as PlayerInitialSpawn is called before the player is ready and therefore LSCS will automatically sync the inventory when the player is ready.
+
+	-- ply:Give("weapon_lscs") -- only needed if they dont have SWEP spawn permission from your admin mod.
+						-- If they have permission they will from now on always spawn with this lightsaber unless they unequip it.
+
+	-- If you give the items outside PlayerInitialSpawn you will need to call ply:lscsCraftSaber() to force the player to craft a lightsaber
+end )
+
+]]
