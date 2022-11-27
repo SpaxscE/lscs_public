352e4afc981c052bd6880573c25bf45ab3a3fd7d
Luna <swagnemite@gmx.de>
2022-11-27 19:51:02 +0100

    changed run and walk anims for butterfly stance

--- lscs_public/lua/wos/anim_extension/holdtypes/lscs_holdtypes.lua
+++ lscs_public/lua/wos/anim_extension/holdtypes/lscs_holdtypes.lua
@@ -5,4 +5,6 @@ DATA.HoldType = "lscs_butterfly"
 DATA.BaseHoldType = "melee"
 DATA.Translations = {} 
 DATA.Translations[ ACT_MP_STAND_IDLE ] = "vanguard_f_idle"
+DATA.Translations[ ACT_MP_WALK ] = "walk_knife"
+DATA.Translations[ ACT_MP_RUN ] = "run_knife"
 wOS.AnimExtension:RegisterHoldtype( DATA )
