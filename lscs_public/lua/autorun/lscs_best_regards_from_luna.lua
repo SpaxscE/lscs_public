f0922a6985c62853fc0502f6e50583ec23fb6407
Luna <swagnemite@gmx.de>
2022-11-27 20:03:16 +0100

    increment version number

--- lscs_public/lua/autorun/lscs_best_regards_from_luna.lua
+++ lscs_public/lua/autorun/lscs_best_regards_from_luna.lua
@@ -10,7 +10,7 @@ best regards luna
 
 LSCS = istable( LSCS ) and LSCS or { Hilt = {}, Blade = {}, Stance = {}, Force = {},BulletTracerDeflectable = {} }
 
-LSCS.VERSION = 148
+LSCS.VERSION = 149
 LSCS.VERSION_GITHUB = 0
 LSCS.VERSION_TYPE = ".GIT"
 
