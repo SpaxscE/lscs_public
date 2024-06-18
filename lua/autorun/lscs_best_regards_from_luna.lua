5c0ff626d919c8d7784b33355c516b0cf51944fa
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-06-18 08:53:59 +0200

    Update lscs_best_regards_from_luna.lua

--- lua/autorun/lscs_best_regards_from_luna.lua
+++ lua/autorun/lscs_best_regards_from_luna.lua
@@ -10,7 +10,7 @@ best regards luna
 
 LSCS = istable( LSCS ) and LSCS or { Hilt = {}, Blade = {}, Stance = {}, Force = {},BulletTracerDeflectable = {} }
 
-LSCS.VERSION = 173
+LSCS.VERSION = 174
 LSCS.VERSION_GITHUB = 0
 LSCS.VERSION_TYPE = ".GIT"
 
