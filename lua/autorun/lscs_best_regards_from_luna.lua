a1f069f1604176310915c88a3bd0f2df0d9f5dba
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-08-02 07:42:27 +0200

    increment version numbré

--- lua/autorun/lscs_best_regards_from_luna.lua
+++ lua/autorun/lscs_best_regards_from_luna.lua
@@ -10,7 +10,7 @@ best regards luna
 
 LSCS = istable( LSCS ) and LSCS or { Hilt = {}, Blade = {}, Stance = {}, Force = {},BulletTracerDeflectable = {} }
 
-LSCS.VERSION = 188
+LSCS.VERSION = 189
 LSCS.VERSION_GITHUB = 0
 LSCS.VERSION_TYPE = ".GIT"
 
